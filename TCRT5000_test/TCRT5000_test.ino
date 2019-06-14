a8b41e220beb766ef0f29231dc678cf9442e73ab
TuanANg <tuan15096@gmail.com>
2019-06-14 17:39:33 +0200

    add TCRT5000_test

--- TCRT5000_test/TCRT5000_test.ino
+++ TCRT5000_test/TCRT5000_test.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+  Inspired by AnalogInOutSerial, an example of Arduino
+*/
 
+// These constants won't change. They're used to give names to the pins used:
+const int irInput = A0;  // Analog input pin that TCRT5000 is attached to
+const int ledPin = 13; // Digital output pin that the LED is attached to
+
+int irValue;
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  pinMode(irInput, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // read the analog in value:
+  irValue = analogRead(irInput);
 
+  if(irValue < 512) {
+    digitalWrite(ledPin, HIGH);
+    }
+  else {
+    digitalWrite(ledPin, LOW);
+    }
+
+  // print the results to the Serial Monitor:
+  Serial.print("IR = ");
+  Serial.println(irValue);
+
+  // wait 2 milliseconds before the next loop for the analog-to-digital
+  // converter to settle after the last reading:
+  delay(100);
 }
